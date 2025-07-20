41f6fc04c0b0cba0dcce4d62bf2720f4805fd945
taleh.karimov <taleh.karimov@smartsolutions.az>
2025-07-20 15:23:31 +0400

    Create application layer and add necessary packages

---
+++ src/RentCarServer.Application/Extensions/RegisterServicesExtension.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using RentCarServer.Application.Behaviours;
+
+namespace RentCarServer.Application.Extensions;
+
+public static class RegisterServicesExtension
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cnf =>
+        {
+            cnf.RegisterServicesFromAssembly(typeof(RegisterServicesExtension).Assembly);
+            cnf.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+        });
+
+        services.AddValidatorsFromAssembly(typeof(RegisterServicesExtension).Assembly);
+
+        return services;
+    }
+}
